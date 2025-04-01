687a63d7cdfcb6294ff561c9a7b87f86a2d7be74
Akshay Ram S <96070309+Akshay-Ram-S@users.noreply.github.com>
2025-04-01 14:58:06 +0530

    Task 1 and Task 2 Commit

---
+++ Task_1/Factorial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Factorial
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a positive integer to calculate its factorial:");
+        int number=Convert.ToInt32(Console.ReadLine());
+
+        // Input validation
+        while(true){
+          if(number<0){
+            Console.WriteLine("Enter a positive number");
+            number=Convert.ToInt32(Console.ReadLine());
+          }
+          else{
+            break;
+          }
+        }
+
+        // Factorial calculation using a loop
+        long factorial = 1;
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+
+        Console.WriteLine($"The factorial of {number} is {factorial}");
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Person{
+  string name;
+  int age;
+  public Person(string _name, int _age)
+  {
+    name=_name;
+    age=_age;
+  }
+  static void Introduce()
+  {
+    Console.WriteLine($"Greetings from {name} and I'm {age} years old.");
+  }
+}
+
+class Program
+{
+  static void main()
+  {
+    Person person1 = new Person("John",21);
+    Person person2 = new Person("Jack",19);
+    Person person3 = new Person("Jill",18);
+
+    person1.Introduce();
+    person2.Introduce();
+    person3.Introduce();
+  }
+}
