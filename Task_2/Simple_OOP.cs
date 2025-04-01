df885993b6a791ff0b778e5d4429b718a63fe43b
Akshay Ram S <96070309+Akshay-Ram-S@users.noreply.github.com>
2025-04-01 17:23:06 +0530

    Update Simple_OOP.cs

--- Task_2/Simple_OOP.cs
+++ Task_2/Simple_OOP.cs
@@ -3,21 +3,17 @@
 class Person{
   string name;
   int age;
-  public Person(string _name, int _age)
-  {
+  public Person(string _name, int _age){
     name=_name;
     age=_age;
   }
-  public void Introduce()
-  {
+  public void Introduce(){
     Console.WriteLine($"Greetings from {name} and I'm {age} years old.");
   }
 }
 
-class Program
-{
-  static void Main()
-  {
+class Program{
+  static void Main(){
     Person person1 = new Person("John",21);
     Person person2 = new Person("Jack",19);
     Person person3 = new Person("Jill",18);
