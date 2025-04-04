27593b35e6de8e95783124868aba167a3f11aedb
Akshay Ram S <96070309+Akshay-Ram-S@users.noreply.github.com>
2025-04-04 16:03:00 +0530

    Added Task 9

---
+++ Task_9/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+
+// Step 1: Define the custom attribute
+[AttributeUsage(AttributeTargets.Method)]
+public class RunnableAttribute : Attribute{
+}
+
+// Step 2: Create classes with methods marked with [Runnable]
+public class Task1{
+    [Runnable]
+    public static void ExecuteTask1(){
+        Console.WriteLine("Task A is running...");
+    }
+}
+
+public class Task2{
+    [Runnable]
+    public static void ExecuteTask2(){
+        Console.WriteLine("Task B is running...");
+    }
+}
+
+public class Task3{
+    public static void NotRunnableMethod(){
+        Console.WriteLine("This should not run.");
+    }
+}
+
+class Program{
+    static void Main(string[] args){
+        // Step 3: Use reflection to find and invoke methods with [Runnable]
+        Assembly currentAssembly = Assembly.GetExecutingAssembly();
+        foreach (Type type in currentAssembly.GetTypes()){
+            foreach (MethodInfo method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static)){
+                if (method.GetCustomAttribute<RunnableAttribute>() != null){
+                    // Step 4: Create an instance if the method is not static
+                    object classInstance = null;
+                    if (!method.IsStatic){
+                        classInstance = Activator.CreateInstance(type);
+                    }
+                    // Step 5: Invoke the method
+                    method.Invoke(classInstance, null);
+                }
+            }
+        }
+    }
+}
---
+++ Task_9/README.md
@@ -0,0 +1,35 @@
+## Reflection and Custom Attributes
+
+## Objective
+
+The objective of this project is to build an application that discovers and executes methods based on custom attributes using reflection in C#.
+
+## Concepts Learned
+
+Attributes : Attributes provide a powerful way to associate metadata, or declarative information, with code (assemblies, types, methods, properties, and so on). After you associate an attribute with a program entity, you can query the attribute at run time by using a technique called reflection.
+<br><br>
+Reflection : Reflection objects are used for obtaining type information at runtime. The classes that give access to the metadata of a running program are in the System.Reflection namespace.
+<br><br>
+
+Steps to be followed for custom attribute :
+
+1. Define the custom attribute.
+   <br>
+2. Create classes with methods marked with attributes.
+   <br>
+3. Use reflection to find and invoke methods.
+   <br>
+4. Invoke the methods.
+   <br><br>
+
+Some classes under Reflection are :
+
+1. Type
+   <br>
+2. MemberInfo
+   <br>
+3. MethodInfo
+   <br>
+4. ConstructorInfor
+   <br>
+5. FieldInfo
